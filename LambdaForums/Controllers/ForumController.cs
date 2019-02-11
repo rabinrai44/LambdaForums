032ff4fd552fc08260617bbefb5eee7669220891
Rabin Rai <rabinrai44@gmail.com>
2019-02-10 23:42:04 -0500

    Downgrade the AspNetCore.All version same as other project 2.0.0 and added ForumController, viewModel

---
+++ LambdaForums/Controllers/ForumController.cs
@@ -0,0 +1,33 @@
+﻿using LambdaForums.Data;
+using LambdaForums.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LambdaForums.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+                .Select(forum => new ForumListingModel {
+                    Id = forum.Id,
+                    Name = forum.Title,
+                    Description = forum.Description
+                });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+    }
+}
