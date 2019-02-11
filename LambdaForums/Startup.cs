33ad95e718738bd1383ad47e4da61c7abe85d3bc
Rabin Rai <rabinrai44@gmail.com>
2019-02-10 21:00:22 -0500

    Creating Models

--- LambdaForums/Startup.cs
+++ LambdaForums/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LambdaForums.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using LambdaForums.Data;
-using LambdaForums.Models;
-using LambdaForums.Services;
 
 namespace LambdaForums
 {
