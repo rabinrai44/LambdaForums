a83c01ee97bf19b5bab683245ca873135bb596ef
Rabin Rai <rabinrai44@gmail.com>
2019-02-10 22:54:57 -0500

    Add Forum Services and implement IForum interface

---
+++ LambdaForums.Data/IForum.cs
@@ -0,0 +1,18 @@
+﻿using LambdaForums.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int Id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task updateForumDescription(int forumId, string newDescription);
+    }
+}
