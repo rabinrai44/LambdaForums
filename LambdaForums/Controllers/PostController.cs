0c219bbc9c2a8bcac3510a3fd4efddac7f093d95
Rabin Rai <rabinrai44@gmail.com>
2019-02-20 01:09:40 -0500

    Topic View and Post Controller Created

---
+++ LambdaForums/Controllers/PostController.cs
@@ -0,0 +1,52 @@
+﻿using LambdaForums.Data;
+using LambdaForums.Data.Models;
+using LambdaForums.Models.Post;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaForums.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPost _postService;
+
+        public PostController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var post = _postService.GetById(id);
+            var replies = BuildPostReplies(post.Replies);
+
+            var model = new PostIndexModel
+            {
+               
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorImageUrl = post.User.ProfileImageUrl,
+                AuthorRating = post.User.Rating,
+                Created = post.Created,
+                PostContent = post.Content,
+                Replies = replies
+            };
+            return View(model);
+        }
+
+        private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
+        {
+            return replies.Select(reply => new PostReplyModel
+            {
+                Id = reply.Id,
+                AuthorName = reply.User.UserName,
+                AuthorImageUrl = reply.User.ProfileImageUrl,
+                AuthorRating = reply.User.Rating,
+                Created = reply.Created,
+                ReplyContent = reply.Content
+            });
+        }
+    }
+}
