011cc50cac3f74c336a255397578de598a41bfbc
Rabin Rai <rabinrai44@gmail.com>
2019-02-23 16:58:58 -0500

    Updated Home Controller for displaying forum post logic

--- LambdaForums/Controllers/HomeController.cs
+++ LambdaForums/Controllers/HomeController.cs
@@ -1,18 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LambdaForums.Data;
+using LambdaForums.Data.Models;
+using LambdaForums.Models;
+using LambdaForums.Models.Forum;
+using LambdaForums.Models.Home;
+using LambdaForums.Models.Post;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using LambdaForums.Models;
 
 namespace LambdaForums.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPost _postService;
+
+        public HomeController(IPost postService )
+        {
+            _postService = postService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = BuildHomeIndexModel();
+
+            return View(model);
+        }
+
+        private HomeIndexModel BuildHomeIndexModel()
+        {
+            var latestPosts = _postService.GetLatestPosts(10);
+
+            var posts = latestPosts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorName = post.User.UserName,
+                AuthorId = post.User.Id,
+                AuthorRating = post.User.Rating,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = GetForumListingForPost(post)
+            });
+
+            return new HomeIndexModel
+            {
+                LatestPosts = posts,
+                SearchQuery = ""
+            };
+        }
+
+        private ForumListingModel GetForumListingForPost(Post post)
+        {
+            var forum = post.Forum;
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                ImageUrl = forum.ImageUrl
+            };
         }
 
         public IActionResult About()
